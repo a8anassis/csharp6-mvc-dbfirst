21d525d67ca394ae6f6432e425c60fedd0a95a96
Th. Androutsos <a8anassis@gmail.com>
2024-11-29 19:02:14 +0200

    updates

--- Repositories/IUnitOfWork.cs
+++ Repositories/IUnitOfWork.cs
@@ -2,7 +2,7 @@
 {
     public interface IUnitOfWork
     {
-        // UserRepository ToDo
+        UserRepository UserRepository { get; }
         TeacherRepository TeacherRepository { get; }
         StudentRepository StudentRepository { get; }
         CourseRepository CourseRepository { get; }
