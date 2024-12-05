1b69c3c1e6bf12735781b89495eb8b9490042f07
Th. Androutsos <a8anassis@gmail.com>
2024-12-05 13:07:13 +0200

    fix, sumplify modelstate invalid

--- Controllers/TeacherController.cs
+++ Controllers/TeacherController.cs
@@ -30,17 +30,22 @@ namespace SchoolApp.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> SignUp(TeacherSignupDTO teacherSignupDTO)
+        public async Task<IActionResult> Signup(TeacherSignupDTO teacherSignupDTO)
         {
+            //if (!ModelState.IsValid)
+            //{
+            //    foreach (var entry in ModelState.Values)
+            //    {
+            //        foreach (var error in entry.Errors)
+            //        {
+            //            ErrorArray.Add(new Error("", error.ErrorMessage, ""));
+            //            ViewData["ErrorArray"] = ErrorArray;
+            //        }
+            //    }
+            //    return View(teacherSignupDTO);
+            //}
             if (!ModelState.IsValid)
             {
-                foreach (var entry in ModelState.Values)
-                {
-                    foreach (var error in entry.Errors)
-                    {
-                        ErrorArray.Add(new Error("", error.ErrorMessage, ""));
-                    }
-                }
                 return View(teacherSignupDTO);
             }
 
